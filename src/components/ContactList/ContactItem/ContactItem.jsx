4e7c0acb82b218d086e109f400650436c0ba17be
PrimeGlorious <TheComboPunk@gmail.com>
2023-07-12 13:09:02 +0300

    fix gh-pages

--- src/components/ContactList/ContactItem/ContactItem.jsx
+++ src/components/ContactList/ContactItem/ContactItem.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 import { ContactCard, DeleteBtn } from './ContactItem.styled';
 
 export const ContactItem = ({ name, number, id, onDeleteBtn }) => (
-  <ContactCard><p>{name}</p><p>{number}</p><DeleteBtn onClick={() => {onDeleteBtn(id)}}>Delete</DeleteBtn></ContactCard>
-)
+  <ContactCard><p>{name}</p><p>{number}</p><DeleteBtn onClick={() => { onDeleteBtn(id) }}>Delete</DeleteBtn></ContactCard>
+);
 
 ContactItem.propTypes = {
   name: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   onDeleteBtn: PropTypes.func.isRequired
-}
+};
